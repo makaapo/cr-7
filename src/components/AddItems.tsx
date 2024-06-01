f171de59a852fbf96ab937e0e600cce348e54cf8
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-01 17:10:59 +0600

    Added component OrderDetails and AddItem

---
+++ src/components/AddItems.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Item } from '../types';
+import MenuItem from './MenuItem';
+
+interface AddItemsProps {
+  items: Item[];
+  addItem: (itemName: string) => void;
+}
+
+const AddItems: React.FC<AddItemsProps> = ({items, addItem}) => {
+  return (
+    <div className="AddItems">
+      {items.map(item => (
+        <MenuItem
+          key={item.name}
+          name={item.name}
+          price={item.price}
+          image={item.image}
+          addItem={addItem}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AddItems;
---
+++ src/components/OrderDetails.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import OrderItem from './OrderItem';
+import {Item} from '../types';
+
+interface Props {
+  order: Item[];
+  DeleteItem: (itemName: string) => void;
+}
+
+const OrderDetails: React.FC<Props> = ({order, DeleteItem}) => {
+  if (order.length === 0) {
+    return <div className="OrderDetails">Order is empty! Please add some items!</div>;
+  }
+
+  return (
+    <div className="OrderDetails">
+      {order.map((item, index) => (
+        <OrderItem key={index} item={item} onDelete={() => DeleteItem(item.name)} />
+      ))}
+    </div>
+  );
+};
+
+export default OrderDetails;
